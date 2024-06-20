54fa82bf48e7b3d7bb62769c6d8c883a2d9915d4
Matthew Harris <matt@harris.pro>
2024-06-20 15:08:49 -0600

    Adjust prod env variables

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.friendsdiscount.ca/api/',
-    wsUrl: 'ws://localhost:8000/'
+    apiUrl: 'https://friendsdiscount.ca/api/',
+    wsUrl: 'ws://friendsdiscount.ca:8003/'
   };
